93191bffb3d0aace8148dcd15ad8c8587c1fb892
= <=>
2024-06-15 17:39:18 +1000

    Empty repo

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ChatBotComponent from './components/ChatBotComponent'
+
+// Creates the chatbot app
+function App() {
+  return (
+    <body>
+    <div className="container">
+      <ChatBotComponent />
+    </div>
+    </body>
+  );
+}
+
+export default App;
