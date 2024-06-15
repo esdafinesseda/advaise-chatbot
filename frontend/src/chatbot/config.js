985ef4a46111e38320e3d7f3169d8267e3a772da
= <=>
2024-06-15 17:39:45 +1000

    Removed file due to front end problem

--- frontend/src/chatbot/config.js
+++
@@ -1,9 +0,0 @@
-import { createChatBotMessage } from 'react-chatbot-kit'
-
-// Chatbot style / initial configurations
-const config = {
-    initialMessages: [createChatBotMessage('Hello! Enter a staff query:')],
-    botName: 'DemoCo Bot'
-}
-
-export default config;
